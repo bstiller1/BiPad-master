85929f2fc4c5ca9f28c18d1e00390dca05b86dab
Blake Stiller <stiller.blake@gmail.com>
2017-01-10 14:42:34 -0500

    BiPad
    Added functionality for Font Colours and Font Family.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace BiPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.formatToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.newToolStripMenuItem.ToolTipText = "Start New File";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.openToolStripMenuItem.ToolTipText = "Open File";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.saveToolStripMenuItem.ToolTipText = "Save File";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.closeToolStripMenuItem.ToolTipText = "Close Application";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colourToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Fo&rmat";
+            // 
+            // colourToolStripMenuItem
+            // 
+            this.colourToolStripMenuItem.Name = "colourToolStripMenuItem";
+            this.colourToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.colourToolStripMenuItem.Text = "Co&lour";
+            this.colourToolStripMenuItem.Click += new System.EventHandler(this.colourToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.fontToolStripMenuItem.Text = "Fon&t";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.aboutToolStripMenuItem.Text = "&Help";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(118, 22);
+            this.aboutToolStripMenuItem1.Text = "&About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(401, 340);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 364);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BiPad
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.SelectionFont = new Font("Courier New", 12, FontStyle.Regular);
+            richTextBox1.SelectionColor = System.Drawing.Color.Black;
+        }
+        /// <summary>
+        /// Global Variable FileName
+        /// </summary>
+        public string FileName;
+
+        // File -> Close - File Menu Action
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            // Create StreamWriter Object to open the file
+            using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+            {
+                
+                FileName = openFileDialog1.FileName;
+                // Read the entire file into buffer
+                String lineChk = sr.ReadToEnd();
+           
+                // Put the file contents in the richTextBox1
+                if (FileName != null && richTextBox1.Text.Equals(lineChk))
+                {
+                    MessageBox.Show("They don't match");
+                }
+            
+
+            if (FileName == null)
+                {
+                    // Display a MsgBox asking the user to save changes or abort. 
+                    if (MessageBox.Show("Do you want to save changes to your text?", "BiPad",
+                       MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        // Open Save As Dialog
+                        SaveFileAs();
+                    }
+ 
+                    else
+                    {
+                        // The user clicked NO and wants to exit the application. 
+                        // Close the StreamReader
+                        sr.Close();
+                        // Garbage Collection
+                        GC.Collect();
+                        // Close everything down.
+                        Application.Exit();
+
+                        
+                    }
+                }
+                else if ((FileName == null) && (richTextBox1.Text == ""))
+                {
+                    // The user wants to exit the application. There is no FilName and the richTextBox is empty. 
+                    // Close everything down.
+                    // Close the StreamReader
+                    sr.Close();
+                    // Garbage Collection
+                    GC.Collect();
+                    Application.Exit();
+                  
+                }
+                else
+                {
+                    // The user wants to exit the application. Close everything down.
+                    // Close the StreamReader
+                    sr.Close();
+                    // Garbage Collection
+                    GC.Collect();
+                    Application.Exit();
+                     
+                }
+            } // Close the StreamReader
+        } //Close menu strip
+
+        // File -> File Save - File Menu Action
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // If the Global FileName exists
+            if (FileName != null)
+            {
+                
+                // Write text to file
+                using (StreamWriter sw = new StreamWriter(FileName))
+                {
+                    sw.WriteLine(richTextBox1.Text);
+
+                    // Close the StreamWriter
+                    sw.Close();
+                    // Garbage Collection
+                    GC.Collect();
+                }
+            }
+            else
+            {
+                // Open Save As Dialog
+                SaveFileAs();
+             }
+
+        }
+
+        // File -> Save As - File Menu Action
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Open Save As Dialog
+            SaveFileAs();
+        }
+
+        // File -> Open - File Menu Action
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (FileName != null)
+            {
+                // Open last directory opened
+                openFileDialog1.RestoreDirectory = true;
+            }
+            else
+            {
+                // Open Default Directory "My Documents"
+                openFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+            
+            // Open Type: All Files and Text .txt Files
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+
+            // If the user clicks OK
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Create StreamWriter Object to open the file
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        // setting the global variable
+                        FileName = openFileDialog1.FileName;
+                        // Read the entire file into buffer
+                        String line = sr.ReadToEnd();
+                        // Put the file contents in the richTextBox1
+                        richTextBox1.Text = line;
+                        // Close the StreamReader
+                        sr.Close();
+                        // Garbage Collection
+                        GC.Collect();
+                    }
+                }
+                    // If there is any problem reading the file
+                catch (Exception ex)
+                {
+                    MessageBox.Show("The file could not be read:" + ex.Message, "Cannot read the File");
+                }
+            }
+            else
+            {
+                // Close Dialog and Reset
+                openFileDialog1.Reset();
+            }
+        }
+
+        
+        /// <summary>
+        /// Save As Method:
+        /// Invokes the Save As Dialog
+        /// </summary>
+ 
+        public void SaveFileAs()
+        {
+            //  This is for "Save As" dialog
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (FileName == null)
+            {
+                // Default FileName
+                saveFileDialog1.FileName = "*.txt";
+            }
+            else
+            {
+                // Default FileName
+                saveFileDialog1.FileName = FileName;
+            }
+            // Save as Type: All Files and Text .txt Files
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; 
+            // Default File Extension
+            saveFileDialog1.DefaultExt = "txt";
+
+            if (FileName != null)
+            {
+                // Open last directory opened
+                saveFileDialog1.RestoreDirectory = true;
+            }
+            else
+            {
+                // Open Default Directory "My Documents"
+                saveFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+            saveFileDialog1.FilterIndex = 2;
+ 
+            // If user clicked OK
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                // Create StreamWriter Object to write the file
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.WriteLine(richTextBox1.Text);
+                    // setting the global variable
+                    FileName = saveFileDialog1.FileName;
+
+                    // Close the StreamWriter
+                    sw.Close();
+                    // Garbage Collection
+                    GC.Collect();
+                }
+
+            }
+            else
+            {
+                // Close Dialog and Reset
+                saveFileDialog1.Reset();
+            }
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileName = null;
+            richTextBox1.Text = "";
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" BiPad Text Editor \r\n Version 1.0 \r\n Blake Stiller Copyright © 2015 \r\n BlakeStiller.com", "About");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" If there are any issues, errors, or suggestions \r\n You can email me at: \r\n me@BlakeStiller.com", "Help");
+        }
+
+        private void colourToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            richTextBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            richTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FileName == null)
+            {
+                SaveFileAs();
+            }
+        }
+    }
+}
+
+
